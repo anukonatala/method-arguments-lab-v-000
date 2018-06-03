bb6edc0b36a3d086bd4f199a76a98ffc498b4f07
Anusha Konatala <anu.konatala@gmail.com>
2018-06-03 06:05:21 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 #introduction("Josh")}.to output("Hi, my name is Josh.\n")
-
+#introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n"
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
